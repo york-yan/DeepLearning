751f643ad152ebac80983553a024262cecb23958
york <649310717@qq.com>
2024-03-06 18:20:31 +0800

    daily push addd ipynb

--- 5_DeepLearn/cfg_manage.py
+++ 5_DeepLearn/cfg_manage.py
@@ -4,6 +4,8 @@ from torch import nn
 net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
 X = torch.rand(size=(2, 4))
 net(X)
-print(net(X))
-print (net[1].state_dict())
-print(net[2].state_dict())
+# print(net(X))
+# print (net[1].state_dict())
+# print(net[2].state_dict())
+# 通过索引访问模型的任意层参数
+print(*[name for name, param in net[0].named_parameters()])
